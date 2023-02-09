5fa62650b2526f3c896f91e25455abb264764f07
Dheeraj <dheerajraina55@gmail.com>
2023-02-09 13:42:23 +0530

    Commit 3
    Adding different colors

---
+++ lib/config/pallete.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Pallete {
+  Color black = Color.fromARGB(255, 34, 33, 33);
+  Color purple =Color.fromARGB(255, 80, 39, 176);
+  Color white =Color.fromARGB(255, 240, 232, 232);
+}
